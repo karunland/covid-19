b4620ad37a3f4ef72fcf1061c7a3b60e15170ccd
Harun Korkmaz <1harunkorkmaz@gmail.com>
2021-04-13 13:06:03 +0300

    Reformat the file

--- covid_lib.py
+++ covid_lib.py
@@ -9,16 +9,16 @@ class covid:
                  my_country='Turkey',
                  country_list='https://api.covid19api.com/countries'):
 
-        self.data_covid     = json.loads(urlopen(covid_url + my_country).read().decode("utf-8"))
+        self.data_covid = json.loads(urlopen(covid_url + my_country).read().decode("utf-8"))
         self.json_countries = json.loads(urlopen(country_list).read().decode("utf-8"))
 
-        self.countries = [item['Country']   for item in self.json_countries]
+        self.countries = [item['Country'] for item in self.json_countries]
         self.confirmed = [item['Confirmed'] for item in self.data_covid]
         self.recovered = [item['Recovered'] for item in self.data_covid]
-        self.date      = [item['Date']      for item in self.data_covid]
-        self.deaths    = [item['Deaths']    for item in self.data_covid]
+        self.date = [item['Date'] for item in self.data_covid]
+        self.deaths = [item['Deaths'] for item in self.data_covid]
 
-        self.death_daily     = self.daily_indormation(self.deaths)
+        self.death_daily = self.daily_indormation(self.deaths)
         self.recovered_daily = self.daily_indormation(self.recovered)
         self.confirmed_daily = self.daily_indormation(self.confirmed)
 
@@ -26,17 +26,17 @@ class covid:
         self.length = len(self.deaths)
 
         self.confirmed_percent_recovered = []
-        self.confirmed_percent_death     = []
+        self.confirmed_percent_death = []
 
         for item in range(len(self.confirmed)):
             try:
-                #self.confirmed_percent_death.append((self.death_daily[item] / self.confirmed_daily[item]) * 100)
-                #self.confirmed_percent_recovered.append((self.recovered_daily[item] / self.confirmed_daily[item]) * 100)
+                # self.confirmed_percent_death.append((self.death_daily[item] / self.confirmed_daily[item]) * 100)
+                # self.confirmed_percent_recovered.append((self.recovered_daily[item] / self.confirmed_daily[item]) * 100)
                 self.confirmed_percent_death.append((self.deaths[item] / self.confirmed[item]) * 100)
                 self.confirmed_percent_recovered.append((self.recovered[item] / self.confirmed[item]) * 100)
-            except ZeroDivisionError: # Avoid the zero division problem
+            except ZeroDivisionError:  # Avoid the zero division problem
                 pass
-                #out of range error
+                # out of range error
                 self.confirmed_percent_death.append(0)
                 self.confirmed_percent_recovered.append(0)
 
@@ -51,7 +51,7 @@ class covid:
 
     # API doesn't include death day by day, need to calculate
 
-    def daily_indormation(self, list=None):
+    def daily_information(self, list=None):
         daily_list = [0]
         length = len(self.deaths)
 
@@ -88,20 +88,21 @@ class covid:
         if args == 2:
             my_plot.plot(self.confirmed_percent_recovered)
             for item in range(self.length):
-                print(f'{self.date[item]} : recovered people = {self.recovered_daily[item]} %{self.confirmed_percent_recovered[item]} people in {self.confirmed_daily}')
+                print(
+                    f'{self.date[item]} : recovered people = {self.recovered_daily[item]} %{self.confirmed_percent_recovered[item]} people in {self.confirmed_daily}')
             my_plot.show()
             my_plot.close()
 
         if args == 3:
             my_plot.plot(self.confirmed_percent_death)
-            for item in range(self.length-1):
-                print(f'{self.date[item+1]} : Today died {self.death_daily[item]}')
+            for item in range(self.length - 1):
+                print(f'{self.date[item + 1]} : Today died {self.death_daily[item]}')
             my_plot.show()
             my_plot.close()
         if args == 4:
             my_plot.plot(self.confirmed_percent_death)
             my_plot.plot(self.confirmed_percent_recovered)
-            for item in range(self.length-1):
+            for item in range(self.length - 1):
                 print(f'{self.date[item + 1]} : Today died {self.death_daily[item]} : Recovered {self.recovered[item]}')
             my_plot.show()
             my_plot.close()
